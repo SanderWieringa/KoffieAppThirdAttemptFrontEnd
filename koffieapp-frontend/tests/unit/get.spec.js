339de747ed341b787dd53ddf745d545375862b35
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-06-25 18:49:39 +0200

    tests added, dockerignore added, dockerfile added

--- koffieapp-frontend/tests/unit/get.spec.js
+++ koffieapp-frontend/tests/unit/get.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
+import ProductTable from '@//components/ProductTable.vue'
+import CardBody from '@//components/CardBody.vue'
 
 describe('App.vue', () => {
     test('setup correctly', () => (
@@ -7,6 +9,16 @@ describe('App.vue', () => {
     ))
     it('can find element', () => {
         const wrapper = shallowMount(App)
-        expect(wrapper.find('h3')).toBeTruthy()
+        const productTable = wrapper.findComponent(ProductTable)
+        const cardBody = wrapper.findComponent(CardBody)
+
+        expect(wrapper.find('ProductTable')).toBeTruthy()
+        expect(wrapper.findAll('div').exists()).toBe(true)
+        expect(productTable.exists()).toBe(true)
+        expect(cardBody.exists()).toBe(true)
+    })
+    it('does exist', () => {
+        const wrapper = shallowMount(App)
+        expect(wrapper.exists('ProductTable')).toBe(true)
     })
 })
